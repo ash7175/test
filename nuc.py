66fce62ff7b2380205080376096246f247807553
ash7175 <tashinh63@gmail.com>
2025-02-08 00:41:09 +0600

    made it short and better

--- nuc.py
+++ nuc.py
@@ -1,5 +1,5 @@
 import random
-
+import collections
 nucleotides = ['A', 'T', 'G', 'C']
 def validate_seq(seq):
     temp_seq = seq.upper()
@@ -11,15 +11,16 @@ def validate_seq(seq):
 def count_nuc(seq):
     seq = seq.upper()
     validate_seq(seq)
-    temp_freq_dict = {'A':0,
-                      'T':0,
-                      'G':0,
-                      'C':0
-                      }
-    for nuc in seq:
-        temp_freq_dict[nuc] += 1
-    return temp_freq_dict
-    #return dict(collections.Counter(seq))
+    # temp_freq_dict = {'A':0,
+    #                   'T':0,
+    #                   'G':0,
+    #                   'C':0
+    #                   }
+    # for nuc in seq:
+    #     temp_freq_dict[nuc] += 1
+    # return temp_freq_dict
+    #introducing Counter function from collections that creates dictionary of each character as key and number of appearance as value
+    return dict(collections.Counter(seq))
     
     
 
